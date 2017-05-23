08caf35daa54ae108851f27390b8beb14ae65713
Komatipolu Mokshagna Sai Teja <mokshagnasaiteja517@gmail.com>
2017-05-23 19:16:36 +0530

    new page added

--- app/server.js
+++ app/server.js
@@ -5,7 +5,10 @@ app.get('/', function (req, res) {
   console.log('Request received');
   res.send('Hello world Once again!');
 });
-
+app.get('/welcome', function (req, res) {
+  console.log('Request received');
+  res.send('Welcome to the next page');
+});
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
