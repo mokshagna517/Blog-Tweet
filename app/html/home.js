c3bb3d32564cc032d21204c521a257bf9e3fb779
Komatipolu Mokshagna Sai Teja <mokshagnasaiteja517@gmail.com>
2017-06-06 16:53:10 +0530

    logout

---
+++ app/html/home.js
@@ -0,0 +1,23 @@
+console.log("login");
+function myfunc(){
+  var request=new XMLHttpRequest;
+   request.onreadystatechange=function(){
+              if(request.readyState===XMLHttpRequest.DONE){
+                  if(request.status===200){
+                      console.log("user logged out");
+                      alert(" user logged out successfully");
+                      }
+                      else{
+                          alert("Error ");
+                      }
+                      
+              }  
+          }; 
+   
+    
+     
+   request.open('POST','http://auth.vcap.me/user/logout',true);
+   request.setRequestHeader('Content-Type', 'application/json');
+   request.send(null);
+    
+ }
