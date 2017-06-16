6d96b661141a953bd19363bcb9541ef97fd3666d
Komatipolu Mokshagna Sai Teja <mokshagnasaiteja517@gmail.com>
2017-06-16 16:57:13 +0530

    Haasura

--- app/server.js
+++ app/server.js
@@ -82,6 +82,9 @@ app.get('/profile', function(req,res){
 	console.log("profile page requested");
 	res.sendFile(path.join(__dirname,'/html','profile.html'));
 });
+app.get('/comment.html', function(req,res){
+	res.sendFile(path.join(__dirname, '/html','comment.html'));
+});
 app.get('/user', function(req,res){
 	res.sendFile(path.join(__dirname,'/html','user.html'));
 });
